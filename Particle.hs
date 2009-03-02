c4d2fec5b8781f7afba723212be6220937c6f3e1
abez <abram.hindle@softwareprocess.us>
2009-03-02 14:56:53 -0500

    stupid sand sorta assy need way better collisions

--- Particle.hs
+++ Particle.hs
@@ -3,12 +3,12 @@ import Data.SpacePart.AABB
 import Data.SpacePart.QuadTree as Quad
 
 
-data Particle = Dust Double Double | Wall Double Double deriving (Eq,Ord,Show)
+data Particle = Dust Double Double Double Double | Wall Double Double deriving (Eq,Ord,Show)
 
 radius_dust = 2.5 :: Double
 radius_wall = 5.0 :: Double
 
-radius (Dust _ _) = radius_dust
+radius (Dust _ _ _ _) = radius_dust
 radius (Wall _ _) = radius_wall
 
 
@@ -20,7 +20,7 @@ particleBoundaryPointsGeneric dust_radius x y =
      ((x + dust_radius), (y + dust_radius)),
      ((x + dust_radius), (y - dust_radius))]
 
-particleBoundaryPoints (Dust x y) = particleBoundaryPointsGeneric radius_dust x y
+particleBoundaryPoints (Dust x y _ _) = particleBoundaryPointsGeneric radius_dust x y
 particleBoundaryPoints (Wall x y) = particleBoundaryPointsGeneric radius_wall x y
 
 particleBoundaryEdgesGeneric dust_radius x y =
@@ -30,18 +30,18 @@ particleBoundaryEdgesGeneric dust_radius x y =
      (((x + dust_radius), (y + dust_radius)), ((x + dust_radius), (y - dust_radius))),
      (((x + dust_radius), (y - dust_radius)), ((x - dust_radius), (y - dust_radius)))]
 
-particleBoundaryEdges (Dust x y) = particleBoundaryEdgesGeneric radius_dust x y
+particleBoundaryEdges (Dust x y _ _) = particleBoundaryEdgesGeneric radius_dust x y
 particleBoundaryEdges (Wall x y) = particleBoundaryEdgesGeneric radius_wall x y
 
 particleBoundaryExtentsGeneric radius x y = (((x - radius),(y - radius)),((x+radius),(y+radius)))
 
-particleBoundaryExtents (Dust x y) = particleBoundaryExtentsGeneric radius_dust x y
+particleBoundaryExtents (Dust x y _ _) = particleBoundaryExtentsGeneric radius_dust x y
 particleBoundaryExtents (Wall x y) = particleBoundaryExtentsGeneric radius_wall x y
 
 particleBoundaryGeneric radius x y = Boundary { boundary_corner = (x - radius, y - radius),
                                                 boundary_size = 2*radius }
 
-particleBoundary (Dust x y) = particleBoundaryGeneric radius_dust  x y
+particleBoundary (Dust x y _ _) = particleBoundaryGeneric radius_dust  x y
 particleBoundary (Wall x y) = particleBoundaryGeneric radius_wall x y
 
 
--- Sand.hs
+++ Sand.hs
@@ -4,13 +4,14 @@ import Graphics.UI.SDL as SDL
 import Graphics.UI.SDL.Image as Image
 import Monad
 import Particle
+import Data.SpacePart.QuadTree as Quad
 
 maxHeight = 480 :: Int
 maxWidth = 640 :: Int
 
 background = "background.png"
 tinyBall = "ball10x10.png"
-
+tinyBlock = "block10x10.png"
 
 -- data Particle = Dust Int Int | Wall Int Int
 data Mousestate = Mouse { left :: Bool,
@@ -25,8 +26,13 @@ defaultMousestate = Mouse { left = False, middle = False, right = False, wheelDo
 data Gamestate = Game { 
       gUpdate :: Bool,
       gMouse :: Mousestate,
-      gParticles :: [Particle]
+      gParticles :: [Particle],
+      gFixed :: [Particle],
+      gFixedQuad ::  QuadTree Particle
     }
+
+baseGameState = Game { gUpdate = True, gMouse = defaultMousestate, gParticles = [],
+                  gFixed = [], gFixedQuad = Quad.empty}
 
 roundInt = fromIntegral . toInteger . floor 
 
@@ -36,13 +42,16 @@ sand_main = do
   screen <- getVideoSurface
   back <- Image.load background
   ball <- Image.load tinyBall
+  block <- Image.load tinyBlock
   blitSurface back Nothing screen Nothing
   SDL.flip screen
-  let startState = Game { gUpdate = True, gMouse = defaultMousestate , gParticles = [] }
-  let drawParticle (Dust x y) = blitSurface ball Nothing screen (Just (Rect (roundInt x) (roundInt y) 10 10))
+  let startState = baseGameState
+  let drawParticle (Dust x y _ _) = blitSurface ball Nothing screen (Just (Rect (roundInt x) (roundInt y) 10 10))
+      drawParticle (Wall x y) = blitSurface block Nothing screen (Just (Rect (roundInt x) (roundInt y) 10 10))
   let drawFrame state = do
               blitSurface back Nothing screen Nothing
               Monad.sequence_ (map drawParticle $ gParticles state)
+              Monad.sequence_ (map drawParticle $ gFixed state)
               SDL.flip screen
               return (state{ gUpdate = False })
   let eventLoop state = do               
@@ -57,10 +66,12 @@ sand_main = do
                            let ry = fromIntegral y
                            let newstate = if (left (gMouse state)) 
                                           then (addParticleToState ButtonLeft rx ry state) 
-                                          else state
+                                          else if (right (gMouse state)) 
+                                               then addWall ButtonRight rx ry state
+                                               else state
                            eventLoop newstate
-                    (MouseButtonUp x y ButtonLeft) -> do
-                           eventLoop (state{ gMouse = ((gMouse state){left = False}) })
+                    (MouseButtonUp x y b) -> do
+                           eventLoop (state{ gMouse = updateMouseUp b (gMouse state) })
                     (MouseButtonDown x y b@ButtonLeft) -> do
                            let rx = fromIntegral x
                            let ry = fromIntegral y
@@ -78,33 +89,86 @@ sand_main = do
                              
   eventLoop startState
 
-
-genericAddParticleToState p ButtonLeft rx ry state =
+updateMouseDown ButtonLeft mouse = mouse{ left = True }
+updateMouseDown ButtonRight mouse = mouse{ right = True }
+updateMouseUp ButtonLeft mouse = mouse{ left = False }
+updateMouseUp ButtonRight mouse = mouse{ right = False }
+    
+addParticleToState b@ButtonLeft rx ry state =
     (state{ gUpdate = True, gMouse = mb,  gParticles = particles}) where
-        newParticle = p (fromIntegral rx) (fromIntegral ry)
+        xvel = if (mod rx 2 == 0) then 0.01 else (-0.01)
+        newParticle = Dust (fromIntegral rx) (fromIntegral ry) xvel  0
         particles = newParticle : (gParticles state)
-        mb = (gMouse state){left = True}
+        mb = updateMouseDown b (gMouse state)
 
-addParticleToState ButtonLeft rx ry state =
-    (state{ gUpdate = True, gMouse = mb,  gParticles = particles}) where
-        newParticle = Dust (fromIntegral rx) (fromIntegral ry)
-        particles = newParticle : (gParticles state)
-        mb = (gMouse state){left = True}
+
+
+addWall button rx ry state = state{ gUpdate = True, 
+                                    gFixed = newFixed, 
+                                    gFixedQuad = newTree,
+                                    gMouse = mb
+                                  }
+    where newParticle = Wall (fromIntegral rx) (fromIntegral ry)
+          newFixed = newParticle : (gFixed state)
+          newTree = Quad.insert newParticle (gFixedQuad state)
+          mb = updateMouseDown button (gMouse state)
+
+
+
 
 
     
 
-particleX (Dust x _) = x
-particleY (Dust x y) = y
-particleXY (Dust x y) = (x,y)
+particleX (Dust x _ _ _) = x
+particleY (Dust x y _ _) = y
+particleXY (Dust x y _ _) = (x,y)
 
-translateParticle x y (Dust x' y') = Dust (x + x') (y + y')
+translateParticle x y (Dust x' y' vx vy) = Dust (x + x') (y + y') vx vy
+pushParticle velx vely (Dust x' y' velx' vely') = Dust (x'+velx) (y'+vely) (velx'+velx) (vely'+vely)
 
-isFixed Wall _ _ = True
+isFixed (Wall _ _) = True
 isFixed _ = False
 
+doesParticleCollide state particle = 
+    case collidesWith of
+      [] -> False
+      _  -> True
+    where collidesWith = Quad.query (Particle.particleBoundary particle) (gFixedQuad state)
+
+
+-- No collisions
+singleParticleStep (Dust x y velx vely) = Dust (x + velx) (y + vely) velx vely
+
+accelerate ax ay (Dust x y vx vy) = Dust (x + ax + vx) (y + ay + vy) (vx + ax) (ay + vy)
+reflect loss (Dust x y vx vy) = Dust (x + rvx) (y + rvy) rvx rvy
+    where rvx = -1.0 * vx * loss
+          rvy = -1.0 * vy * loss
+reflectY loss (Dust x y vx vy) = Dust (x + rvx) (y + rvy) rvx rvy
+    where rvx = 1.001 * vx
+          rvy = -1.0 * vy * loss
+
+
+particleStep state d@(Dust x y velx vely) =
+    case (doesParticleCollide state newDust) of
+      False -> newDust
+      True  -> case (collidesLeft, collidesRight) of
+                 (False, False) -> reflect 0.9 d
+                 (True,False) -> dustRight
+                 (False,True) -> dustLeft
+                 (True,True) -> reflect 0.9 d
+               --  (False,True) -> 
+               --  (False,False) -> Dust (x + velx) y velx vely
+                                  
+    where newDust  = accelerate 0 0.01 d
+          dustLeft :: Particle
+          dustLeft = accelerate (-1) 0 d
+          dustRight = accelerate 1 0 d          
+          collidesLeft = doesParticleCollide state dustLeft
+          collidesRight = doesParticleCollide state dustRight
+
+
 particleStateUpdate state = state{gUpdate = True, gParticles = filteredParticles } where
-    translatedParticles = map (translateParticle 0 1) (gParticles state)
+    translatedParticles = map (particleStep state) (gParticles state)
     filteredParticles = filter (\x -> (particleY x < (fromIntegral maxHeight))) translatedParticles
 
 main = sand_main
